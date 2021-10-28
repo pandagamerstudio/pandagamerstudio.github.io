8f7b06061335f8aa8c4ba65151f2782633887ba2
Raulr00 <57095736+Raulr00@users.noreply.github.com>
2021-10-28 16:41:56 +0200

    Carrusel de miembros del equipo, plantillas creadas de los miembros, estructura de los trabajos previos
    Queda completar la información de los miembros del equipo

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@
 */
 window.addEventListener('DOMContentLoaded', event => {
 
+
+
     const sidebarWrapper = document.getElementById('sidebar-wrapper');
     let scrollToTopVisible = false;
     // Closes the sidebar menu
